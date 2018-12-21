bd79e736bec93ad5c8ff2e5bab2d3ce55c30d3c8
zhanglei <zhanglei@i-counting.cn>
2018-12-21 11:45:35 +0800

    [feat] 增加用户相关接口

--- src/main/java/com/zeal/zealsay/controller/UserController.java
+++ src/main/java/com/zeal/zealsay/controller/UserController.java
@@ -8,6 +8,7 @@ import com.zeal.zealsay.common.entity.Result;
 import com.zeal.zealsay.converter.UserConvertMapper;
 import com.zeal.zealsay.dto.request.UserAddRequest;
 import com.zeal.zealsay.dto.request.UserPageRequest;
+import com.zeal.zealsay.dto.request.UserUpdateRequest;
 import com.zeal.zealsay.dto.response.UserResponse;
 import com.zeal.zealsay.entity.User;
 import com.zeal.zealsay.helper.UserHelper;
@@ -17,6 +18,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -24,14 +26,14 @@ import java.util.Collection;
 /**
  * 用户服务入口.
  *
- * @author  zhanglei
+ * @author zhanglei
  * @date 2018/11/15  6:56 PM
  */
 @Api(tags = "用户模块")
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/user")
-public class UserController  {
+public class UserController {
 
   @Autowired
   UserService userService;
@@ -47,7 +49,7 @@ public class UserController  {
    * @date 2018/9/7  下午6:00
    */
   @GetMapping("/{id}")
-  @ApiOperation(value = "根据id获取用户信息",notes = "根据id获取用户信息")
+  @ApiOperation(value = "根据id获取用户信息", notes = "根据id获取用户信息")
   public Result<UserResponse> getById(@PathVariable String id) {
     log.info("开始查询用户id为 '{}' 的用户信息", id);
     return Result
@@ -61,10 +63,10 @@ public class UserController  {
    * @date 2018/9/7  下午6:00
    */
   @GetMapping("/page")
-  @ApiOperation(value = "分页获取用户信息列表",notes = "分页获取用户信息列表")
+  @ApiOperation(value = "分页获取用户信息列表", notes = "分页获取用户信息列表")
   public Result<PageInfo<UserResponse>> getByPaginate(@Value("1") Long pageNumber,
-                                                                      @Value("10") Long pageSize,
-                                                                      UserPageRequest userPageRequest) {
+                                                      @Value("10") Long pageSize,
+                                                      UserPageRequest userPageRequest) {
     log.info("开始进行分页查询用户列表，查询参数为 '{}' ", userPageRequest);
     Page<User> userPage = (Page<User>) userService
         .page(new Page<>(pageNumber, pageSize), new QueryWrapper(userConvertMapper
@@ -76,27 +78,53 @@ public class UserController  {
   /**
    * 批量禁用用户.
    *
-   * @author  zhanglei
+   * @author zhanglei
    * @date 2018/11/15  8:24 PM
    */
-  @PutMapping("disable")
-  @ApiOperation(value = "根据id列表批量禁用用户",notes = "根据id列表批量禁用用户")
-    public Result<Boolean> markUserDisabled(@RequestBody Collection<String> ids) {
-        log.info("开始执行对用户 id 在 '{}' 内的用户执行批量封禁操作", ids.toString());
-        return Result.ok();
-    }
+  @PutMapping("disable/batch")
+  @ApiOperation(value = "根据id列表批量禁用用户", notes = "根据id列表批量禁用用户")
+  public Result<Boolean> markUserDisabledBatch(@RequestBody Collection<Long> ids) {
+    log.info("开始执行对用户 id 在 '{}' 内的用户执行批量封禁操作", ids.toString());
+    return Result.of(userService.markUserDisabledBatch(ids));
+  }
 
-    /**
-    * 添加用户.
-    *
-    * @author  zeal
-    * @date 2018/11/24 13:50
-    */
-    @PutMapping("")
-    @ApiOperation(value = "管理员添加用户",notes = "管理员添加用户")
-    public Result<Boolean> addUser(@RequestBody UserAddRequest userAddRequest) {
-        log.info("开始执行添加用户逻辑,新增加用户的参数为：‘{}’", userAddRequest);
-        return Result.ok();
-    }
+  /**
+   * 禁用单条用户.
+   *
+   * @author zhanglei
+   * @date 2018/11/15  8:24 PM
+   */
+  @PutMapping("disable/{id}")
+  @ApiOperation(value = "根据id禁用用户", notes = "根据id禁用用户")
+  public Result<Boolean> markUserDisabled(@PathVariable Long id) {
+    log.info("开始执行对用户 id 为 '{}' 的用户执行封禁操作", id);
+    return Result.of(userService.markUserDisabled(id));
+  }
+
+  /**
+   * 添加用户.
+   *
+   * @author zeal
+   * @date 2018/11/24 13:50
+   */
+  @PostMapping("")
+  @ApiOperation(value = "管理员添加用户", notes = "管理员添加用户")
+  public Result<Boolean> addUser(@RequestBody @Validated UserAddRequest userAddRequest) {
+    log.info("开始执行添加用户逻辑,新增加用户的参数为：‘{}’", userAddRequest);
+    return Result.of(userService.addUser(userAddRequest));
+  }
+
+  /**
+   * 修改用户.
+   *
+   * @author zeal
+   * @date 2018/11/24 13:50
+   */
+  @PutMapping("")
+  @ApiOperation(value = "管理员修改用户信息", notes = "管理管理员修改用户信息员添加用户")
+  public Result<Boolean> updateUser(@RequestBody @Validated UserUpdateRequest userUpdateRequest) {
+    log.info("开始执行修改用户信息逻辑,需要修改的用户的参数为：‘{}’", userUpdateRequest);
+    return Result.of(userService.updateUser(userUpdateRequest));
+  }
 }
 
