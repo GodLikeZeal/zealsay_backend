afa9bcc33dcf6f55ee89dbc33be15eb97e4a39a5
zhanglei <zhanglei@i-counting.cn>
2019-03-09 17:58:35 +0800

    [feat] 模糊查询用户列表

--- src/main/java/com/zeal/zealsay/helper/UserHelper.java
+++ src/main/java/com/zeal/zealsay/helper/UserHelper.java
@@ -1,6 +1,6 @@
 package com.zeal.zealsay.helper;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zeal.zealsay.common.entity.PageInfo;
 import com.zeal.zealsay.converter.UserConvertMapper;
@@ -8,11 +8,13 @@ import com.zeal.zealsay.dto.request.UserAddRequest;
 import com.zeal.zealsay.dto.request.UserUpdateRequest;
 import com.zeal.zealsay.dto.response.UserResponse;
 import com.zeal.zealsay.entity.User;
+import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -65,5 +67,55 @@ public class UserHelper {
    */
   public User initBeforeAdd(UserAddRequest userAddRequest) {
     return userConvertMapper.toUser(userAddRequest);
+  }
+
+  /**
+   * 构造模糊查询参数.
+   *
+   * @author  zhanglei
+   * @date 2019-03-08  11:55
+   */
+  public QueryWrapper<User> buildVagueQuery(@NonNull User user) {
+    QueryWrapper<User> queryWrapper = new QueryWrapper();
+    if (StringUtils.isNotBlank(user.getAddress())) {
+      queryWrapper.like("address",user.getAddress());
+    }
+    if (StringUtils.isNotBlank(user.getArea())) {
+      queryWrapper.like("area",user.getArea());
+    }
+    if (StringUtils.isNotBlank(user.getUsername())) {
+      queryWrapper.like("username",user.getUsername());
+    }
+    if (StringUtils.isNotBlank(user.getName())) {
+      queryWrapper.like("name",user.getName());
+    }
+    if (StringUtils.isNotBlank(user.getLabel())) {
+      queryWrapper.like("label",user.getLabel());
+    }
+    if (StringUtils.isNotBlank(user.getPhoneNumber())) {
+      queryWrapper.like("phone_number",user.getPhoneNumber());
+    }
+    if (StringUtils.isNotBlank(user.getEmail())) {
+      queryWrapper.like("email",user.getEmail());
+    }
+    if (StringUtils.isNotBlank(user.getAddress())) {
+      queryWrapper.like("address",user.getAddress());
+    }
+    if (StringUtils.isNotBlank(user.getProvince())) {
+      queryWrapper.like("province",user.getProvince());
+    }
+    if (StringUtils.isNotBlank(user.getCity())) {
+      queryWrapper.like("city",user.getCity());
+    }
+    if (StringUtils.isNotBlank(user.getAddress())) {
+      queryWrapper.like("address",user.getAddress());
+    }
+    if (!Objects.isNull(user.getAge())) {
+      queryWrapper.like("age",user.getAge());
+    }
+    if (!Objects.isNull(user.getSex())) {
+      queryWrapper.like("sex",user.getSex());
+    }
+    return queryWrapper;
   }
 }
