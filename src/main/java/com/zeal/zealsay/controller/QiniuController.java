d6f32c0421b6d85f8abab5a04a22b81393c8752e
zhanglei <zhanglei@i-counting.cn>
2019-04-29 19:32:29 +0800

    [feat] 批量上传图片上传

--- src/main/java/com/zeal/zealsay/controller/QiniuController.java
+++ src/main/java/com/zeal/zealsay/controller/QiniuController.java
@@ -1,33 +1,21 @@
 package com.zeal.zealsay.controller;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.qiniu.http.Response;
-import com.zeal.zealsay.common.entity.PageInfo;
+
+import com.google.common.collect.Lists;
 import com.zeal.zealsay.common.entity.Result;
 import com.zeal.zealsay.common.third.qiniu.QiniuService;
-import com.zeal.zealsay.converter.ArticleConvertMapper;
-import com.zeal.zealsay.dto.request.ArticleAddRequest;
-import com.zeal.zealsay.dto.request.ArticlePageRequest;
-import com.zeal.zealsay.dto.request.ArticleUpdateRequest;
-import com.zeal.zealsay.dto.response.ArticleResponse;
-import com.zeal.zealsay.entity.Article;
 import com.zeal.zealsay.exception.ServiceException;
-import com.zeal.zealsay.helper.ArticleHelper;
-import com.zeal.zealsay.service.ArticleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
+import java.util.List;
 
 /**
  * <p>
@@ -47,7 +35,7 @@ public class QiniuController {
   QiniuService qiniuService;
 
   /**
-   * 根据id来查询.
+   * 上传图片.
    *
    * @author zhanglei
    * @date 2018/9/7  下午6:00
@@ -67,6 +55,31 @@ public class QiniuController {
       throw new ServiceException("上传文件失败");
     }
 
+  }
+
+  /**
+   * 批量上传图片.
+   *
+   * @author zhanglei
+   * @date 2018/9/7  下午6:00
+   */
+  @PostMapping("/upload/multiple")
+  @ApiOperation(value = "批量上传文件", notes = "上传文件")
+  public Result<List<String>> uploadMultiple(@RequestParam MultipartFile[] files) {
+    if (files.length <= 0) {
+      throw new ServiceException("上传文件失败");
+    }
+    List<String> results = Lists.newArrayList();
+    for (MultipartFile file : files) {
+      try (InputStream in = file.getInputStream()) {
+        log.info("开始上传文件到七牛云");
+        results.add(qiniuService.uploadFile(in, qiniuService.createFileName(file)));
+      } catch (IOException e) {
+        log.error("上传文件到七牛云失败!");
+        throw new ServiceException("上传文件失败");
+      }
+    }
+    return Result.of(results);
   }
 
 
