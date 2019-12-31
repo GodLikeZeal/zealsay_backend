3629da067362f6ecb9d2e91e1fe1e68f34450d6a
zhanglei <zhanglei@i-counting.cn>
2019-12-31 19:38:34 +0800

    [feat] 优化喜欢逻辑

--- src/main/java/com/zeal/zealsay/service/BlockLogService.java
+++ src/main/java/com/zeal/zealsay/service/BlockLogService.java
@@ -1,19 +1,23 @@
 package com.zeal.zealsay.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zeal.zealsay.common.constant.enums.BlockAction;
 import com.zeal.zealsay.common.constant.enums.BlockType;
+import com.zeal.zealsay.common.entity.SecuityUser;
 import com.zeal.zealsay.entity.Article;
 import com.zeal.zealsay.entity.BlockLog;
 import com.zeal.zealsay.entity.User;
 import com.zeal.zealsay.exception.ServiceException;
 import com.zeal.zealsay.mapper.BlockLogMapper;
 import com.zeal.zealsay.service.auth.UserDetailServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -24,12 +28,15 @@ import java.util.stream.Collectors;
  * @author zhanglei
  * @since 2019-03-16
  */
+@Slf4j
 @Transactional(rollbackFor = {ServiceException.class, RuntimeException.class, Exception.class})
 @Service
 public class BlockLogService extends AbstractService<BlockLogMapper, BlockLog> {
 
   @Autowired
   UserDetailServiceImpl userDetailService;
+  @Autowired
+  UserService userService;
 
   /**
    * 封禁解封记录.
@@ -116,5 +123,31 @@ public class BlockLogService extends AbstractService<BlockLogMapper, BlockLog> {
         .targetName(user.getTitle())
         .build())
         .collect(Collectors.toList()));
+  }
+
+  /**
+   * 获取当前用户的动态信息.
+   *
+   * @author  zhanglei
+   * @date 2019-12-31  12:15
+   */
+  public List<BlockLog> getCurrentUserActions() {
+    //获取当前用户
+    User user = userService.getById(userDetailService.getCurrentUser().getUserId());
+    if (Objects.isNull(user)) {
+      return null;
+    }
+    log.info("获取当前用户 '{} ' 的动态", user.getUsername());
+    List<BlockLog> list = list(new QueryWrapper<BlockLog>()
+        .eq("operator_id",user.getId())
+        .orderByDesc("operator_date"));
+    //增加注册信息
+    list.add(BlockLog.builder()
+        .type(BlockType.USER)
+        .action(BlockAction.REGISTER)
+        .operatorName(user.getUsername())
+        .operatorDate(user.getRegisterDate())
+        .build());
+    return list;
   }
 }
